1f8280c1f03e1cb8bf1dc4911b9ec89944d1cf91
Rahul masal <rahulmasal2013@gmail.com>
2026-04-28 19:39:06 +0530

    Enable APK splits for different architectures (ABI)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,15 @@ android {
         versionName = "1.0.0"
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
